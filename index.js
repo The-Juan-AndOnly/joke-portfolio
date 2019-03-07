22d884bc096435599a3fb8356557911f29953a1a
Juan Lopez <juanl0215@gmail.com>
2019-03-07 00:37:52 -0500

    Adding responsive media queries

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ const formBtn = document.querySelector('.submitBtn');
 const modal = document.querySelector('#modal');
 const modalContent = document.querySelector('.modal-content');
 const closeBtn = document.querySelector('.close');
+const mobileMenuBtn = document.querySelector('.mobile-nav');
+const mobileLinks = document.querySelector('.mobile-links');
 
 const onFormSubmit = e => {
   e.preventDefault;
   modal.style.display = 'block';
 };
+
+mobileMenuBtn.addEventListener('click', () => {
+  mobileLinks.classList.toggle('show');
+
+  if (mobileLinks.className.includes('show')) {
+    document.querySelector('header').style.height = 'calc(100vh + 196px)';
+  }
+});
 
 formBtn.addEventListener('click', onFormSubmit);
 
